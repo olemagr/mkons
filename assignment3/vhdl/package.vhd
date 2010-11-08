55d2d49208cc30b83dfaa3e8f092b05bac832827
Ole Magnus Ruud <olemagr@stud.ntnu.no>
2010-11-08 20:27:45 +0100

    Some new types, alu function constants for testing. Component declarations

--- assignment3/vhdl/package.vhd
+++ assignment3/vhdl/package.vhd
@@ -27,6 +27,9 @@ package dmkons_package is
   constant IADDR_BUS : integer := 8;                  -- do not change this
   -- number of bits in instruction memory data bus (= instr. word size)
   constant IDATA_BUS : integer := 32;                 -- do not change this
+  
+  -- number of bits in data memory address bus
+  constant DADDR_BUS : integer := 8;                  -- do not change this
   -- number of bits in databus and registers
   constant DDATA_BUS : integer := 8;                  -- max 32
   
@@ -39,6 +42,9 @@ package dmkons_package is
 
   -- number of words in instruction memory
   constant IMEM_SIZE : integer := 2 ** IADDR_BUS;     -- do not change this
+  -- number of words in data memory
+  constant DMEM_SIZE : integer := 2 ** IADDR_BUS;     -- do not change this
+  
   -- number of bits used for selecting registers from regfile
   constant REG_ADDR_BUS   : integer := 4;             -- do not change this
   -- total number of registers in register file
@@ -51,6 +57,11 @@ package dmkons_package is
   constant LDI				: op_type := "010";
   constant LOAD			: op_type := "110";
   constant STORE 			: op_type := "100";
+  -- Alu functions
+  subtype alu_funct is std_logic_vector(funct_bus -1 downto 0);
+  constant ALU_MOVE		: alu_funct := "0000";
+  constant ALU_AND		: alu_funct := "0001";
+  constant ALU_ADD		: alu_funct := "0111";
 
   -----------------------------------------------------------------------------
   -- types
@@ -173,6 +184,153 @@ package dmkons_package is
       rst        : in  std_logic;
       core_clk   : in  std_logic);
   end component;
+  
+  component alu
+
+  port (
+    alu_funct : in std_logic_vector(FUNCT_BUS - 1 downto 0);
+    alu_in_a  : in std_logic_vector(DDATA_BUS - 1 downto 0);
+    alu_in_b  : in std_logic_vector(DDATA_BUS - 1 downto 0);
+
+    alu_out    : out std_logic_vector(DDATA_BUS - 1 downto 0);
+    alu_status : out std_logic_vector(STATUS_BUS - 1 downto 0));
+
+end component alu;
+
+component control
+  
+  port (
+    -- Inputs
+    IF_ID_valid         : in    std_logic;
+    IF_ID_opcode        : in    op_type;
+    IF_ID_Ra            : in    std_logic_vector(REG_ADDR_BUS - 1 downto 0);
+    IF_ID_Rb            : in    std_logic_vector(REG_ADDR_BUS - 1 downto 0);
+    ID_EX_Rd            : in    std_logic_vector(REG_ADDR_BUS - 1 downto 0);
+    ID_EX_write_reg     : in    std_logic;
+    EX_WB_Rd            : in    std_logic_vector(REG_ADDR_BUS - 1 downto 0);
+    EX_WB_write_reg     : in    std_logic;
+    zf_post_mux         : in    std_logic;
+
+    -- Outputs
+    pc_mux              : out   std_logic;
+	 pc_enable				: out   std_logic;
+    set_IF_ID_valid     : out   std_logic;
+    id_op_a_mux         : out   std_logic;
+    id_op_b_mux         : out   std_logic;
+    EX                  : out   EX_control;
+    WB                  : out   WB_control
+    
+    
+);
+
+end component control;
+
+component ex_operand_mux
+   port(
+      op_a_mux      : in std_logic;
+      op_b_mux      : in std_logic;
+      wb_data       : in std_logic_vector(DDATA_BUS - 1 downto 0); 
+      idex_data_a    : in std_logic_vector(DDATA_BUS - 1 downto 0); 
+      idex_data_b    : in std_logic_vector(DDATA_BUS - 1 downto 0); 
+      op_a          : out std_logic_vector(DDATA_BUS - 1 downto 0); 
+      op_b          : out std_logic_vector(DDATA_BUS - 1 downto 0)
+	);
+end component ex_operand_mux;
+
+component ex_wb_reg
+   port(
+      core_rst      : in std_logic;
+      core_clk      : in std_logic;
+      reg_in        : in ex_wb;
+      reg_out       : out ex_wb
+	);
+end component ex_wb_reg;
+
+component id_ex_reg 
+   port(
+      core_rst      : in std_logic;
+      core_clk      : in std_logic;
+      reg_in        : in id_ex;
+      reg_out       : out id_ex
+	);
+end component id_ex_reg;
+
+component id_operand_mux 
+   port(
+      op_a_mux      : in std_logic;
+      op_b_mux      : in std_logic;
+      wb_data       : in std_logic_vector(DDATA_BUS - 1 downto 0); 
+      reg_data_a    : in std_logic_vector(DDATA_BUS - 1 downto 0); 
+      reg_data_b    : in std_logic_vector(DDATA_BUS - 1 downto 0); 
+      op_a          : out std_logic_vector(DDATA_BUS - 1 downto 0); 
+      op_b          : out std_logic_vector(DDATA_BUS - 1 downto 0)
+	);
+end component id_operand_mux;
+
+component if_id_reg 
+
+   port(
+      core_rst      : in std_logic;
+      core_clk      : in std_logic;
+      valid			  : in std_logic;
+		ir            : in std_logic_vector( IDATA_BUS - 1 downto 0 );
+		if_id_out     : out if_id
+	);
+
+end component if_id_reg;
+
+component pc 
+  
+  port (
+    -- PC input
+    pc_in           	: in std_logic_vector(IADDR_BUS - 1 downto 0);
+    pc_write_enable 	: in std_logic;
+    pc_mux_sel 	        : in std_logic;
+    -- PC output to memory
+    pc_out          	: out std_logic_vector(IADDR_BUS - 1 downto 0);
+    -- Global clock and reset
+    core_rst      		: in std_logic;
+    core_clk        	: in std_logic);
+
+end component pc;
+
+component regfile_mux 
+  Port ( selector : in  wb_mux_type;
+         immediate : in  STD_LOGIC_VECTOR(DDATA_BUS - 1 downto 0);
+         alu_out : in  STD_LOGIC_VECTOR(DDATA_BUS - 1 downto 0);
+			mem_out : in  STD_LOGIC_VECTOR(DDATA_BUS - 1 downto 0);
+         data_d : out  STD_LOGIC_VECTOR(DDATA_BUS - 1 downto 0));
+end component regfile_mux;
+
+component sr 
+  Port ( 
+    -- SR input
+    sr_in           : in std_logic_vector(STATUS_BUS - 1 downto 0);
+    sr_write_enable : in std_logic;
+    -- SR output
+    sr_out          : out std_logic_vector(STATUS_BUS - 1 downto 0);
+    -- Global clock and reset
+    core_rst        : in std_logic;
+    core_clk			 : in std_logic);
+end component sr;
+
+component cpu is
+      port (
+         -- imem write port
+         imem_w_address : in std_logic_vector(IADDR_BUS - 1 downto 0);
+         imem_w_data    : in std_logic_vector(IDATA_BUS - 1 downto 0);
+         imem_w_enable  : in std_logic;
+
+         -- register values in regfile
+         reg_values : out regfile_type;
+
+         -- clock used for cpu core
+         core_clk : in std_logic;
+         -- reset used for cpu core
+         core_rst : in std_logic;
+   		-- BenERA LEDS
+	   	led : out std_logic_vector(15 downto 0));	 
+   end component;
 
   -----------------------------------------------------------------------------
   -- global signals for testbench
