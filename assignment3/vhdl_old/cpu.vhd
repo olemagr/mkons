e4d989656fa1ccbcac2f117a0f112c471cc12e86
Ole Magnus Ruud <olemagr@stud.ntnu.no>
2010-11-15 17:23:57 +0100

    Renamed old vhdl folder

--- assignment3/vhdl_old/cpu.vhd
+++ assignment3/vhdl_old/cpu.vhd
@@ -1,32 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    16:29:35 11/08/2010 
--- Design Name: 
--- Module Name:    cpu - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use work.dmkons_package.all;
-
----- Uncomment the following library declaration if instantiating
----- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity cpu is
 	port (
